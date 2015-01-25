7231116262ef16d7bf20f98c75bb087763b5e478
Ken Cluff <ken@ktcsoftware.com>
2015-01-25 06:11:57 -0700

    Initial Commit

---
+++ ApologistToolKit/ApologistToolKit.h
@@ -0,0 +1,19 @@
+//
+//  ApologistToolKit.h
+//  ApologistToolKit
+//
+//  Created by CustomSoftware on 1/25/15.
+//  Copyright (c) 2015 CustomSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApologistToolKit.
+FOUNDATION_EXPORT double ApologistToolKitVersionNumber;
+
+//! Project version string for ApologistToolKit.
+FOUNDATION_EXPORT const unsigned char ApologistToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApologistToolKit/PublicHeader.h>
+
+
